c7bada231d77dfae045755858f9251b1cd98614e
Alexandra <alexandraavendano@github.com>
2021-08-04 15:54:34 -0700

    Add logger and change the getMethod.

--- src/main/java/com/practicespring/printerstore/controllers/ClientController.java
+++ src/main/java/com/practicespring/printerstore/controllers/ClientController.java
@@ -7,9 +7,15 @@ import com.practicespring.printerstore.service.ClientServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class ClientController {
+
+    static final Logger LOGGER = Logger.getLogger(EmployeeController.class.getName());
+
     private final ClientServices clientServices;
 
     @Autowired
@@ -18,13 +24,14 @@ public class ClientController {
     }
 
 
-    @GetMapping("/users/{id}")
-    Client getClient(@PathVariable String id) {
+    @GetMapping("/users")
+    Client getClient(@RequestParam String id) {
         return clientServices.findBy(id).orElseThrow(() -> new ClientNotFoundException(id));
     }
 
     @PostMapping("/users")
     Client createClient(@RequestBody Client newClient) {
+        LOGGER.log(Level.INFO, "Saving a new client");
         Role role = new Role("client");
         newClient.setRole(role);
         return clientServices.create(newClient);
