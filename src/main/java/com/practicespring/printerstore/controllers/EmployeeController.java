a5eb7209cf5c92cc45bcf7ad611ab3f87f72ebfb
Alexandra <alexandraavendano@github.com>
2021-08-16 11:58:04 -0700

    Changes: - Add get all employees. - Not return password in the json. - Add encryption when a new employee is created.

--- src/main/java/com/practicespring/printerstore/controllers/EmployeeController.java
+++ src/main/java/com/practicespring/printerstore/controllers/EmployeeController.java
@@ -2,18 +2,28 @@ package com.practicespring.printerstore.controllers;
 
 import com.practicespring.printerstore.exceptions.EmployeeNotFoundException;
 import com.practicespring.printerstore.models.Employee;
-import com.practicespring.printerstore.models.Role;
 import com.practicespring.printerstore.service.EmployeeServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employees")
 @CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
-    private final EmployeeServices employeeServices;
 
-    public EmployeeController(EmployeeServices employeeServices){
+    private final EmployeeServices employeeServices;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public EmployeeController(EmployeeServices employeeServices, BCryptPasswordEncoder bCryptPasswordEncoder){
         this.employeeServices = employeeServices;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @GetMapping("")
+    Iterable<Employee> getAll(){
+        return employeeServices.findAll();
     }
 
     @GetMapping("/{id}")
@@ -23,6 +33,7 @@ public class EmployeeController {
 
     @PostMapping("/signup")
     Employee create(@RequestBody Employee newEmployee){
+        newEmployee.setPassword(bCryptPasswordEncoder.encode(newEmployee.getPassword()));
         return employeeServices.create(newEmployee);
     }
 
